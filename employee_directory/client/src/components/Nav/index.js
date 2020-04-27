2eb627e10c3fa7864798fbf4b86990ea8a4b2c4e
Spencer Wood <spencer.wood@me.com>
2020-04-26 21:52:20 -0500

    created nav and index

---
+++ employee_directory/client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Nav() {
+
+    return (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <a className='navbar-brand' href='/'>
+                Employee Directory
+            </a>
+        </nav>
+    );
+}
+
+export default Nav;
